7807cc4131d3d98a2596a17a2a7bb68cbc338119
jahir-tawhid <jahirtawhid1988@gmail.com>
2025-12-31 22:14:05 +0600

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,51 @@
 import { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
+import { Link } from "react-router";
 
 
 const Navbar = () => {
 
                 const [isOpen, setIsOpen] = useState(false);
-
                 const toggleMenu = () => {
                                 setIsOpen(!isOpen);
                 }
 
-
   return (
-    <nav className="bg-green-800 text-white py-4 md:py-8">
+    <nav className="bg-green-800 text-white py-4 md:py-8 relative">
                 <div className="container mx-auto flex justify-between items-center">
-                                <h3> React Practice</h3>
+                                <h3 className="text-xl font-bold"> React Practice</h3>
 
                                  {/* Mobile menu Bar */}
-                                <div onClick={toggleMenu}> 
-                                { isOpen ? <IoMdClose /> : <FaBars />}
-                                
-                                
+                                <div className="md:hidden">
+                                 <button onClick={toggleMenu}>
+                                  {
+                                     isOpen ? <IoMdClose /> : <FaBars />
+                                  }
+                                 </button>
                                 </div>
-                                <ul className="flex space-x-4 md:space-x-6">
+                                <ul className="hidden md:flex space-x-4 md:space-x-6">
+                                                <li><Link>Home</Link></li>
+                                                <li><Link>Products</Link></li>
+                                                <li><Link>Blogs</Link></li>
+                                                <li><Link>Contact</Link></li>
+                                                <li><Link>About</Link></li>
+                                </ul>
+                         <button className="hidden md:block"> LogIn </button>
+
+                         {/* Mobile menu collapsed */}
+                         <div className={`md:hidden w-full absolute bg-green-600 top-full left-0 ${isOpen ? 'block' : 'hidden'}`}>
+                          <ul className="flex flex-col items-center py-4">
                                                 <li>Home</li>
                                                 <li>Products</li>
                                                 <li>Blogs</li>
                                                 <li>Contact</li>
                                                 <li>About</li>
+                                                <li>
+                                                  <button > LogIn </button>
+                                                </li>
                                 </ul>
-                         <button> LogIn </button>
+                         </div>
                 </div>
     </nav>
   )
