367daa2f36d313d85d52908330b448028734324f
jahir-tawhid <jahirtawhid1988@gmail.com>
2026-01-01 20:38:27 +0600

    firebase added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -145,7 +145,7 @@ const Navbar = () => {
   return (
     <nav className="bg-green-800 text-white py-4 md:py-8 sticky top-0 z-10 border-b-2 border-green-500">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <h3 className="text-xl font-bold"> React Practice</h3>
+        <h3 className="text-xl font-bold"> Simple React </h3>
 
         {/* Mobile menu Bar */}
         <div className="md:hidden">
